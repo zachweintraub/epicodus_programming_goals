bb47f0078ed9b4ca6443c3a38060254fd44a3ec8
zach_weintraub <zachweintraub@gmail.com>
2019-02-02 13:02:06 -0800

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+alert("Hello World");
+$(document).ready(function() {
+  $("#goal-button").click(function() {
+    $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+    $("#new-goal").val("");
+  })
+})
